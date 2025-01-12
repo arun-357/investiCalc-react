515ec2b462e02de20428b4f4f6a662c73bcedfa4
arun-357s <arunachalamnachiappan20@gmail.com>
2025-01-12 13:38:44 +0530

    lift:state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import "./index.css"
+import { useState } from "react"
 
 import Header from "./components/Header"
 import User from "./components/User"
+import Results from "./components/Results"
 
 function App() {
+  // States
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1000,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  // Helpers 
+  function handleChange(identifier, value) {
+    setUserInput(prev => {
+          return { ...prev, [identifier]: +value };
+    });
+  }
+
   return <>
     <Header />
-    <User />
+    <User onChange={handleChange} userInput={userInput}/>
+    <Results input={userInput}/>
   </>
 }
 
